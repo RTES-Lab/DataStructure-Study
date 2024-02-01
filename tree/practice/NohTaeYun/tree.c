d75156265216cde52ba76804c3cdb363f6436fbc
taeyunnn <155617166+taeyunnn@users.noreply.github.com>
2024-02-01 15:29:28 +0900

    Update tree.c

--- tree/practice/NohTaeYun/tree.c
+++ tree/practice/NohTaeYun/tree.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define Maxsize 200001
+#include <stdlib.h>
 
 typedef struct tree{
     int left;
@@ -9,20 +9,18 @@ typedef struct tree{
 int main(void){
     unsigned int n; //노드의 개수
     unsigned long k;
-    Tree arr[Maxsize];
 
     scanf(" %u",&n);
+    Tree * arr = malloc(sizeof(Tree) * (n+1));
     for(int i = 1;i <= n;i++){
-        scanf(" %d %d",&arr[i].left,&arr[i].right); //편의상 인덱스가 1인 원소를 1번 노드라 하자.
+        scanf(" %u %u",&arr[i].left,&arr[i].right); //편의상 인덱스가 1인 원소를 1번 노드라 하자.
     }
     scanf(" %lu",&k);
     unsigned int now = 1; 
-    while(1){
-        // 자식 노드가 없는 경우
-        if(arr[now].left == -1 && arr[now].right == -1)
-            break;
+    // 구슬 이동(자식 노드가 0개, 즉 잎에 도달하면 멈춤)
+    while(arr[now].left != -1 || arr[now].right != -1){
         // 자식 노드가 1개인 경우
-        else if(arr[now].left == -1)
+        if(arr[now].left == -1)
             now = arr[now].right;
         else if(arr[now].right == -1)
             now = arr[now].left;
@@ -36,6 +34,7 @@ int main(void){
             k = k / 2;
         }                
     }
+		free(arr);
     printf("%u\n",now);
     return 0;
 }
